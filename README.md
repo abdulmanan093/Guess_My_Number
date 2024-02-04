984444e07d5272c724584ff468de4a32cb5acb4d
Abdul Manan <138243512+ABDULMANAN7893@users.noreply.github.com>
2024-02-04 13:35:39 +0500

    Initial commit 🚀

---
+++ README.md
@@ -0,0 +1,10 @@
+# GUESS MY NUMBER!
+
+Guess the number! game created with javascript <br>
+To view the project click here [Demo](https://guess-my-number-mannan.netlify.app/)
+![image](https://github.com/abdulmanan093/Facebook_Login_Clone-Tailwind-CSS/assets/138243512/66d4d2ed-9017-4747-8d0a-3b82442c3a8b)
+
+## Features:
+
+- Run the game in live server
+- Enjoy the game
---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const checkNumber = function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('⛔ No number');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number');
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High' : '📉 Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+};
+
+document.querySelector('.check').addEventListener('click', checkNumber);
+
+document.querySelector('.guess').addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    checkNumber();
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
